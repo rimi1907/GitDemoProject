ba261601553426199210698cd872b9b98d481a1f
gargi <gargi@192.168.2.21>
2024-05-02 21:01:58 -0400

    First Commit

---
+++ src/Demo/Test1.java
@@ -0,0 +1,20 @@
+package Demo;
+
+public class Test1 {
+
+	public static void main(String[] args)
+	{
+		
+		String hungry="yes";
+		 if (hungry=="yes")
+		 {
+			 System.out.println("Eat Food");
+		 }
+		 else
+		 {
+			 System.out.println("Do Work");
+		 }
+			 
+	}
+
+}
